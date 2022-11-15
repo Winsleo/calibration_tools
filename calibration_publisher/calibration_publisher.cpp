992bb5d928610967d72e487487161d553468b5f6
KeepOnKeepOn <longwudi@qq.com>
2022-11-15 17:46:28 +0800

    add manually dynamic calibration reconfigure

--- calibration_publisher/calibration_publisher.cpp
+++ calibration_publisher/calibration_publisher.cpp
@@ -7,7 +7,16 @@
 #include <sensor_msgs/Image.h>
 #include "autoware_msgs/ProjectionMatrix.h"
 
-static cv::Mat CameraExtrinsicMat;
+#include "tools_colored_print.hpp"
+#include <csignal>  
+#include <pcl/common/eigen.h>
+#include <cmath>
+#include <Eigen/Eigen>
+//dynamic reconfigure includes
+#include <dynamic_reconfigure/server.h>
+#include <calibration_publisher/CalibConfig.h>
+
+static Eigen::Matrix4d M_cam2lidar;
 static cv::Mat CameraMat;
 static cv::Mat DistCoeff;
 static cv::Size ImageSize;
@@ -15,6 +24,8 @@ static std::string DistModel;
 
 static ros::Publisher camera_info_pub;
 static ros::Publisher projection_matrix_pub;
+
+static bool instrinsics_parsed_;
 
 static bool isRegister_tf;
 static bool isPublish_extrinsic;
@@ -24,13 +35,29 @@ static std::string camera_id_str_;
 static std::string camera_frame_;
 static std::string target_frame_;
 
-static bool instrinsics_parsed_;
-static bool extrinsics_parsed_;
-
 static sensor_msgs::CameraInfo camera_info_msg_;
 static autoware_msgs::ProjectionMatrix extrinsic_matrix_msg_;
 
-void tfRegistration(const cv::Mat &camExtMat, const ros::Time &timeStamp)
+void SigHandle(int sig)
+{
+    scope_color(ANSI_COLOR_CYAN_BOLD);
+    std::cout<<"Modified extrinsic matrix:"<<std::endl;
+	std::cout<< M_cam2lidar <<std::endl;
+	std::cout<<"Euler angles(RPY): "<< M_cam2lidar.block<3,3>(0,0).eulerAngles(0,1,2).transpose() <<std::endl;
+	std::cout<<"Translation(XYZ):    "<< M_cam2lidar(0,3)<<"   "<<M_cam2lidar(1,3)<<"   "<<M_cam2lidar(2,3) <<std::endl;
+}
+template<typename T>
+T rad2deg(T radians)
+{
+  return radians * 180.0 / M_PI;
+}
+
+template<typename T>
+T deg2rad(T degrees)
+{
+  return degrees * M_PI / 180.0;
+}
+void tfRegistration(const Eigen::Matrix4d &camExtMat, const ros::Time &timeStamp)
 {
 	tf::Matrix3x3 rotation_mat;
 	double roll = 0, pitch = 0, yaw = 0;
@@ -38,34 +65,29 @@ void tfRegistration(const cv::Mat &camExtMat, const ros::Time &timeStamp)
 	tf::Transform transform;
 	static tf::TransformBroadcaster broadcaster;
 
-	rotation_mat.setValue(camExtMat.at<double>(0, 0), camExtMat.at<double>(0, 1), camExtMat.at<double>(0, 2),
-	                      camExtMat.at<double>(1, 0), camExtMat.at<double>(1, 1), camExtMat.at<double>(1, 2),
-	                      camExtMat.at<double>(2, 0), camExtMat.at<double>(2, 1), camExtMat.at<double>(2, 2));
+	rotation_mat.setValue(camExtMat(0, 0), camExtMat(0, 1), camExtMat(0, 2),
+	                      camExtMat(1, 0), camExtMat(1, 1), camExtMat(1, 2),
+	                      camExtMat(2, 0), camExtMat(2, 1), camExtMat(2, 2));
 
 	rotation_mat.getRPY(roll, pitch, yaw, 1);
 
 	quaternion.setRPY(roll, pitch, yaw);
 
-	transform.setOrigin(
-			tf::Vector3(camExtMat.at<double>(0, 3), camExtMat.at<double>(1, 3), camExtMat.at<double>(2, 3)));
+	transform.setOrigin(tf::Vector3(camExtMat(0, 3), camExtMat(1, 3), camExtMat(2, 3)));
 
 	transform.setRotation(quaternion);
 
 	broadcaster.sendTransform(tf::StampedTransform(transform, timeStamp, target_frame_, camera_frame_));
 }
 
-void projectionMatrix_sender(const cv::Mat &projMat, const ros::Time &timeStamp)
+void projectionMatrix_sender(const Eigen::Matrix4d &projMat, const ros::Time &timeStamp)
 {
-	if (!extrinsics_parsed_)
+	for (int row = 0; row < 4; row++)
 	{
-		for (int row = 0; row < 4; row++)
+		for (int col = 0; col < 4; col++)
 		{
-			for (int col = 0; col < 4; col++)
-			{
-				extrinsic_matrix_msg_.projection_matrix[row * 4 + col] = projMat.at<double>(row, col);
-			}
+			extrinsic_matrix_msg_.projection_matrix[row * 4 + col] = projMat(row, col);
 		}
-		extrinsics_parsed_ = true;
 	}
 	extrinsic_matrix_msg_.header.stamp = timeStamp;
 	extrinsic_matrix_msg_.header.frame_id = camera_frame_;
@@ -130,7 +152,7 @@ static void image_raw_cb(const sensor_msgs::Image &image_msg)
 	/* create TF between velodyne and camera with time stamp of /image_raw */
 	if (isRegister_tf)
 	{
-		tfRegistration(CameraExtrinsicMat, timeStampOfImage);
+		tfRegistration(M_cam2lidar, timeStampOfImage);
 	}
 
 	if (isPublish_cameraInfo)
@@ -139,8 +161,21 @@ static void image_raw_cb(const sensor_msgs::Image &image_msg)
 	}
 	if (isPublish_extrinsic)
 	{
-		projectionMatrix_sender(CameraExtrinsicMat, timeStampOfImage);
+		projectionMatrix_sender(M_cam2lidar, timeStampOfImage);
 	}
+}
+
+void reconfigure_callback(calibration_publisher::CalibConfig& config) {
+	double x = config.x;
+	double y = config.y;
+	double z = config.z;
+	double roll = deg2rad(config.roll);
+	double pitch = deg2rad(config.pitch);
+	double yaw = deg2rad(config.yaw);
+	ROS_INFO("x:%f  y:%f z:%f roll:%f pitch:%f yaw:%f", x, y, z,config.roll, config.pitch, config.yaw);
+	Eigen::Affine3d A_cam2lidar;
+	pcl::getTransformation(x, y, z, roll, pitch, yaw,A_cam2lidar);//由x,y,z平移和roll,pitch,yaw欧拉角旋转(3-2-1姿态序列)生成对应的仿射变换
+	M_cam2lidar = A_cam2lidar.matrix();
 }
 
 int main(int argc, char *argv[])
@@ -170,7 +205,7 @@ int main(int argc, char *argv[])
 	private_nh.param<std::string>("camera_frame", camera_frame_, "camera");
 	ROS_INFO("[%s] camera_frame: '%s'", __APP_NAME__, camera_frame_.c_str());
 
-	private_nh.param<std::string>("target_frame", target_frame_, "velodyne");
+	private_nh.param<std::string>("target_frame", target_frame_, "lidar");
 	ROS_INFO("[%s] target_frame: '%s'", __APP_NAME__, target_frame_.c_str());
 
 	std::string calibration_file;
@@ -190,13 +225,22 @@ int main(int argc, char *argv[])
 		ros::shutdown();
 		return -1;
 	}
-
+	cv::Mat CameraExtrinsicMat;
 	fs["CameraExtrinsicMat"] >> CameraExtrinsicMat;
 	fs["CameraMat"] >> CameraMat;
 	fs["DistCoeff"] >> DistCoeff;
 	fs["ImageSize"] >> ImageSize;
 	fs["DistModel"] >> DistModel;
-
+	  
+  	M_cam2lidar<<CameraExtrinsicMat.at<double>(0, 0), CameraExtrinsicMat.at<double>(0, 1), CameraExtrinsicMat.at<double>(0, 2),CameraExtrinsicMat.at<double>(0, 3),
+	  CameraExtrinsicMat.at<double>(1, 0), CameraExtrinsicMat.at<double>(1, 1), CameraExtrinsicMat.at<double>(1, 2), CameraExtrinsicMat.at<double>(1, 3),
+	  CameraExtrinsicMat.at<double>(2, 0), CameraExtrinsicMat.at<double>(2, 1), CameraExtrinsicMat.at<double>(2, 2), CameraExtrinsicMat.at<double>(2, 3),
+	  0.0, 0.0, 0.0, 1.0;
+  	double x,y,z,roll,pitch,yaw;
+ 	pcl::getTranslationAndEulerAngles(Eigen::Affine3d(M_cam2lidar),x,y,z,roll,pitch,yaw);//获取仿射变换等效的x,y,z平移和欧拉角旋转(3-2-1姿态序列)
+	scope_color(ANSI_COLOR_CYAN_BOLD);
+	std::cout<<"x:"<<x<<" y:"<<y<<" z:"<<z<<" roll:"<< rad2deg(roll) <<" pitch:"<< rad2deg(pitch) <<" yaw:"<< rad2deg(yaw) <<std::endl;
+	
 	std::string image_topic_name;
 	std::string camera_info_name;
 	std::string projection_matrix_topic;
@@ -211,7 +255,6 @@ int main(int argc, char *argv[])
 	ROS_INFO("[%s] projection_matrix_topic: %s", __APP_NAME__, projection_matrix_topic.c_str());
 
 	instrinsics_parsed_ = false;
-	extrinsics_parsed_ = false;
 
 	std::string name_space_str = ros::this_node::getNamespace();
 	if (name_space_str != "/")
@@ -235,7 +278,13 @@ int main(int argc, char *argv[])
 	camera_info_pub = n.advertise<sensor_msgs::CameraInfo>(camera_info_name, 10, true);
 
 	projection_matrix_pub = n.advertise<autoware_msgs::ProjectionMatrix>(projection_matrix_topic, 10, true);
-
+	
+	//动态参数服务器
+	dynamic_reconfigure::Server<calibration_publisher::CalibConfig> server;
+	dynamic_reconfigure::Server<calibration_publisher::CalibConfig>::CallbackType server_callback = boost::bind(&reconfigure_callback, _1);
+	//回调函数与服务端绑定
+	server.setCallback(server_callback);
+	signal(SIGINT, SigHandle);
 	ros::spin();
 
 	return 0;
